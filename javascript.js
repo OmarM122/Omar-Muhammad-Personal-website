e59ade1c6f99f50028e1418b9fcabb56af7aeb15
OmarM122 <omarmuhammad1226@gmail.com>
2023-04-11 19:25:41 -0400

    added contact.html file. Moved the links to other pages using flex

--- javascript.js
+++ javascript.js
@@ -10,4 +10,24 @@ const w = 'website'
 alert(`${wel} ${t} ${m} ${p} ${w} !`)
 console.log('test')
 
+const form = document.querySelector("#form")
+   const submitButton = document.querySelector("#submitButton")
+   const scriptURL = 'https://script.google.com/macros/s/AKfycbxLZTlgHBunzjDAyvzTFB9AAQZS2vGT4hSZKlfMDVMQrrHEQwKDYO2-fo05BNfFegYWVg/exec'
 
+   form.addEventListener('submit', e => {
+     submitButton.disabled = true
+     e.preventDefault()
+     let requestBody = new FormData(form)
+     fetch(scriptURL, { method: 'POST', body: requestBody})
+       .then(response => {
+          alert('Success!', response)
+          submitButton.disabled = false
+          console.log('press')
+         })
+       .catch(error => {
+       alert('Error!', error.message)
+         submitButton.disabled = false
+
+       }
+       )
+   })
